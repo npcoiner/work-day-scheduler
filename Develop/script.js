198b80718ad655e3256ea7dbaa4737ba12ec937e
Nicholas Coiner <npcoiner@ucdavis.edu>
2023-04-22 21:23:54 -0700

    Added saveButton variable

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,12 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
+//unsure whether or not to create the querySelectors inside or outside 
+//the wrapped jQuery call
+
+var saveButton = document.querySelector("#save-button");
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
