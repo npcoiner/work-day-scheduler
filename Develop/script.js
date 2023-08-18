b33ac635cb4279d12d6b07881aed6f978413fbbc
Nicholas Coiner <npcoiner@ucdavis.edu>
2023-08-17 19:10:26 -0700

    Added simple listener pseudo-code

--- Develop/script.js
+++ Develop/script.js
@@ -1,15 +1,11 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-
-//unsure whether or not to create the querySelectors inside or outside 
-//the wrapped jQuery call
-
-var saveButton = document.querySelector(".saveBtn");
-
 //current day formated
 var currentDay = dayjs().format('MMM D, YYYY, hh:mm:ss a');
 var currentHour = dayjs().hour();
+
+
+// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
+// the code isn't run until the browser has finished rendering all the elements
+// in the html.
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -17,8 +13,14 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  saveButton.addEventListener("click", function(event) {
-    event.preventDefault();
+  $('#time-block').on("click", '.saveBtn', function(event) {
+    var timeIndex = parseInt($(this).attr('data-index'));
+    var events = readEventsFromStorage();
+
+    events[timeIndex] = parseString($(this).attr('description'));
+    saveEventsToStorage(events);
+
+    printEventData();
 
 
   });
