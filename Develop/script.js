0680e2128c528c445605e3ec3c3cdf9cdd53dab3
Nicholas Coiner <npcoiner@ucdavis.edu>
2023-08-18 14:57:09 -0700

    Added saveEventsToStorage, getTense, on click event, and for each events for the time-blocks

--- Develop/script.js
+++ Develop/script.js
@@ -13,24 +13,37 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  $('#time-block').on("click", '.saveBtn', function(event) {
-    var timeIndex = parseInt($(this).attr('data-index'));
+  $('.time-block').on("click", '.saveBtn', function(e) {
+    console.log("Hello")
+    var timeIndex = parseInt($(this).attr('id')); //gives id of button
     var events = readEventsFromStorage();
-
+    if (!events){
+      events = [];
+    }
     events[timeIndex] = parseString($(this).attr('description'));
     saveEventsToStorage(events);
 
     printEventData();
-
+    console.log(events);
 
   });
   
   //TODO: Add a function which saves all events to storage
-
+  function saveEventsToStorage(events){
+    localStorage.setItem('events', JSON.stringify(events));
+  }
   //TODO: Add a function which reads all events from storage and displays them
   //onto the page. Should create a div element following the guidelines of the HTML 
   //with an hourly block for each standard business hour
-
+  function readEventsFromStorage(){
+    var events = localStorage.getItem('events');
+    if (events) {
+      events = JSON.parse(events);
+    } else {
+      events = [];
+    }
+    return events;
+  }
 
   
   // TODO: Add code to apply the past, present, or future class to each time
@@ -39,9 +52,24 @@ $(function () {
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
   //
-  function applyTense(){
-
+  function getTense(blockTime){
+    console.log(blockTime);
+    console.log(currentHour)
+    if( blockTime < currentHour){
+      return "past";
+    }
+    if(blockTime == currentHour){
+      return "present";
+    }
+    else{
+      return "future";
+    }
   }
+  $('.time-block').each(function(i) {
+    var blockTime = parseInt($(this).attr('id'));
+    console.log("this" + blockTime)
+    $(this).attr('tense',  getTense(blockTime))
+});
 
 
   // TODO: Add code to get any user input that was saved in localStorage and set
